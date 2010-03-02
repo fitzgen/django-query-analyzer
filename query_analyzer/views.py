48e23f95a1499d9ecf8ae1f908f85f0aac3b6c1e
Nick Fitzgerald <fitzgen@gmail.com>
2010-03-02 15:44:35 -0800

    Making it so that the template will be rendered if there is no sql params

--- query_analyzer/views.py
+++ query_analyzer/views.py
@@ -17,34 +17,39 @@ def analyze(request):
     #hash = sha_constructor(settings.SECRET_KEY + sql + params).hexdigest()
     #if hash != request.GET.get('hash', ''):
         #return HttpResponseBadRequest('Tamper alert') # SQL Tampering alert
-    if sql.lower().strip().startswith('select'):
-        if params:
-            params = simplejson.loads(params)
-        cursor = connection.cursor()
+    if sql != "":
+        if sql.lower().strip().startswith('select'):
+            if params:
+                params = simplejson.loads(params)
+            else:
+                params = {}
+            cursor = connection.cursor()
 
-        # SELECT
-        cursor.execute(sql, params)
-        select_headers = [d[0] for d in cursor.description]
-        select_result = cursor.fetchall()
+            # SELECT
+            cursor.execute(sql, params)
+            select_headers = [d[0] for d in cursor.description]
+            select_result = cursor.fetchall()
 
-        # EXPLAIN
-        if settings.DATABASE_ENGINE == "sqlite3":
-            cursor.execute("EXPLAIN QUERY PLAN %s" % (sql,), params)
+            # EXPLAIN
+            if settings.DATABASE_ENGINE == "sqlite3":
+                cursor.execute("EXPLAIN QUERY PLAN %s" % (sql,), params)
+            else:
+                cursor.execute("EXPLAIN %s" % (sql,), params)
+            explain_headers = [d[0] for d in cursor.description]
+            explain_result = cursor.fetchall()
+
+            cursor.close()
+
+            context = {
+                'select_result': select_result,
+                'explain_result': explain_result,
+                'sql': cursor.db.ops.last_executed_query(cursor, sql, params),
+                'duration': request.GET.get('duration', 0.0),
+                'select_headers': select_headers,
+                'explain_headers': explain_headers,
+            }
         else:
-            cursor.execute("EXPLAIN %s" % (sql,), params)
-        explain_headers = [d[0] for d in cursor.description]
-        explain_result = cursor.fetchall()
-
-        cursor.close()
-
-        context = {
-            'select_result': select_result,
-            'explain_result': explain_result,
-            'sql': cursor.db.ops.last_executed_query(cursor, sql, params),
-            'duration': request.GET.get('duration', 0.0),
-            'select_headers': select_headers,
-            'explain_headers': explain_headers,
-        }
-        return render_to_response('query_analyzer/analyze.html', context)
-    raise InvalidSQLError("Only 'select' queries are allowed.")
-
+            raise InvalidSQLError("Only 'select' queries are allowed.")
+    else:
+        context = {}
+    return render_to_response('query_analyzer/analyze.html', context)

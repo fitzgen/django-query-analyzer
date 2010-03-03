3c2fdaedcb63f2ad91eeeb152eb2ed2648ce7f05
Nick Fitzgerald <fitzgen@gmail.com>
2010-03-03 11:37:13 -0800

    Adding init function to grab all models

--- query_analyzer/templates/query_analyzer/base.js
+++ query_analyzer/templates/query_analyzer/base.js
@@ -17,6 +17,36 @@ function getModelDetails(appLabel, modelName, success, error) {
     });
 }
 
-$(document).ready(function () {
+function initModelSelect(selectElem) {
+    return getModels(
+        function (data, textStatus, xhr) {
+            var appName = function (model) {
+                return model[0];
+            };
+            var modelName = function (model) {
+                return model[1];
+            };
+            $.each(data.models, function (i, model) {
+                console.log(model); // REMOVE
+                console.log(appName(model));
+                console.log(modelName(model));
+                selectElem.tempest(
+                    "append",
+                    "modelOption",
+                    {
+                        model: appName(model) + "." + modelName(model)
+                    }
+                );
+            });
+        },
+        function () {
+            alert("ERRORS!");
+        }
+    );
+}
 
+$(document).ready(function () {
+    var modelSelect = $("dl dd #model-select");
+
+    initModelSelect(modelSelect);
 });

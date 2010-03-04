63cb4bb89c63970ab748c518fe0f45ea41ed8dc9
Nick Fitzgerald <fitzgen@gmail.com>
2010-03-03 16:07:14 -0800

    Getting all the model info via ajax calls

--- query_analyzer/templates/query_analyzer/base.js
+++ query_analyzer/templates/query_analyzer/base.js
@@ -1,3 +1,11 @@
+function log(msg) {
+    if (window.console && window.console.log) {
+        return console.log(msg);
+    } else {
+        return alert(msg);
+    }
+}
+
 function getModels(success, error) {
     return $.ajax({
         dataType: "json",
@@ -17,44 +25,90 @@ function getModelDetails(appLabel, modelName, success, error) {
     });
 }
 
-function log(msg) {
-    if (window.console && window.console.log) {
-        return console.log(msg);
-    } else {
-        return alert(msg);
-    }
+var appName = function (model) {
+    return model[0];
+};
+var modelName = function (model) {
+    return model[1];
+};
+
+function initModelSelect(selectElem, models) {
+    return $.each(models, function (i, model) {
+        selectElem.tempest(
+            "append",
+            "modelOption",
+            {
+                model: appName(model) + "." + modelName(model)
+            }
+        );
+    });
 }
 
-function initModelSelect(selectElem) {
-    return getModels(
-        function (data, textStatus, xhr) {
-            var appName = function (model) {
-                return model[0];
+function getAllModelDetails(models, detailsObj) {
+    var ensureApp = function (appName) {
+        if (typeof detailsObj[appName] === "undefined") {
+            detailsObj[appName] = {};
+        }
+    };
+    var ensureModel = function (appName, modelName) {
+        if (typeof detailsObj[appName][modelName] === "undefined") {
+            detailsObj[appName][modelName] = {
+                managers: [],
+                fields: [],
+                relatedFields: []
             };
-            var modelName = function (model) {
-                return model[1];
-            };
-            $.each(data.models, function (i, model) {
-                console.log(model); // REMOVE
-                console.log(appName(model));
-                console.log(modelName(model));
-                selectElem.tempest(
-                    "append",
-                    "modelOption",
-                    {
-                        model: appName(model) + "." + modelName(model)
-                    }
-                );
-            });
+        }
+    };
+    var makeAdder = function (type) {
+        return function (appName, modelName, value) {
+            detailsObj[appName][modelName][type].push(value);
+        };
+    };
+    var addManager = makeAdder("managers");
+    var addField = makeAdder("fields");
+    var addRelatedField = makeAdder("relatedFields");
+
+    return $.each(models, function (i, model) {
+        var app = appName(model);
+        var thisModel = modelName(model);
+
+        ensureApp(app);
+        ensureModel(app, thisModel);
+
+        getModelDetails(
+            app,
+            thisModel,
+            function (data, textStatus, xhr) {
+                $.each(data.managers, function (i, manager) {
+                    addManager(app, thisModel, manager);
+                });
+                $.each(data.fields, function (i, field) {
+                    addField(app, thisModel, field);
+                });
+                $.each(data.related_fields, function (i, relField) {
+                    addRelatedField(app, thisModel, relField);
+                });
+            },
+            function () {
+                log("Error retreiving model info.");
+            }
+        );
+    });
+}
+
+$(document).ready(function () {
+    var modelSelect = $("dl dd #model-select");
+    var modelsObj = {};
+    window.models = modelsObj; // REMOVE
+
+    getModels(
+        function (data,textStatus, XHR) {
+            initModelSelect(modelSelect, data.models);
+            getAllModelDetails(data.models, modelsObj);
         },
         function () {
             log("Failure fetching models.");
         }
     );
-}
 
-$(document).ready(function () {
-    var modelSelect = $("dl dd #model-select");
-
-    initModelSelect(modelSelect);
 });
